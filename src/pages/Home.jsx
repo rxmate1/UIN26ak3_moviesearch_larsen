8a137161c4507c2d016fef4fe3bb374f93519776
rxmate1 <Marius.w.larsen2@gmail.com>
2026-02-27 16:19:16 +0100

    Arbeidskrav_3 Start
    jobbet i forelesning

---
+++ src/pages/Home.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+export default function Home() {
+const [search, setSearch] = useState("")
+
+const baseUrl = `http://www.omdbapi.com/?s=${search}&apikey=`
+    const apiKey = '8da59edd'
+    const getMovies = async () => {
+        try {
+            const response = await fetch(`${baseUrl}${apiKey}`)
+            const data = await response.json()
+            console.log(data)
+        }
+        catch (err) {
+            console.error(err)
+        }
+    }
+
+    const handleChange = (e)=>{
+    setSearch(e.target.value)
+    }
+
+    return (
+        <main>
+            <h1>Forside</h1>
+            <form>
+                <label>
+                    Søk etter film
+                <input type="search" placeholder="Harry Potter" onChange={handleChange}></input>
+                </label>
+            </form>
+            <button onClick={getMovies}>Søk</button>
+        </main>
+    )
+}
